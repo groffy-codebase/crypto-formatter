fc919397f3e00dd861fe778b7eba683bc110a3f7
loweandaya <loweandaya7566@gmail.com>
2024-05-28 22:55:37 +0800

    fix: typos

--- src/utils/floatable.ts
+++ src/utils/floatable.ts
@@ -1,5 +1,5 @@
 /**
- * 
+ * cleans and checks if `value` is a floatable
  * @param value the value to be checked if floatable string 
  * @param error the error callback if using in frontend
  * @returns the cleaned float number string
